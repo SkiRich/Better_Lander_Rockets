e7b3bb4f383a9f4605f74ffba451619ebd34a038
SkiRich <richard@szkodzinski.com>
2021-09-30 21:58:51 -0400

    Initial Commit

---
+++ metadata.lua
@@ -0,0 +1,13 @@
+return PlaceObj('ModDef', {
+	'title', "Better Lander Rockets",
+	'description', "Better Lander Rockets",
+	'last_changes', "No change",
+	'id', "moSrUdg",
+	'pops_desktop_uuid', "8eaa3645-4bed-4b1c-880d-28d91dc912ca",
+	'pops_any_uuid', "8579c7f2-ec22-4464-beda-db1eaa380cf5",
+	'author', "SkiRich",
+	'version', 3,
+	'lua_revision', 1007000,
+	'saved_with_revision', 1008224,
+	'saved', 1633053301,
+})
