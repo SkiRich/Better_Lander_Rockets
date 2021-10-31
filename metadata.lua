f5fdee362a2b32ccacd9387a4d799bfe1bdd307f
SkiRich <richard@szkodzinski.com>
2021-10-31 05:17:19 -0400

    Upload to steam and pdox

--- metadata.lua
+++ metadata.lua
@@ -8,7 +8,7 @@ return PlaceObj('ModDef', {
 	'pops_desktop_uuid', "8eaa3645-4bed-4b1c-880d-28d91dc912ca",
 	'pops_any_uuid', "8579c7f2-ec22-4464-beda-db1eaa380cf5",
 	'author', "SkiRich",
-	'version', 32,
+	'version', 36,
 	'lua_revision', 1007000,
 	'saved_with_revision', 1009385,
 	'code', {
@@ -16,7 +16,7 @@ return PlaceObj('ModDef', {
 		"Code/BLR_2Panels.lua",
 		"Code/BLR_3ModConfig.lua",
 	},
-	'saved', 1635510877,
+	'saved', 1635671809,
 	'screenshot1', "Images/BLRlogo.png",
 	'screenshot2', "Images/BLRpanel1.png",
 	'screenshot3', "Images/BLRpanel2.png",
